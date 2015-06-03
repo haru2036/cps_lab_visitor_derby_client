bf3f598ce32309d73109d058851b6fbe96a67723
haru2036 <haru2036@gmail.com>
2015-06-03 23:51:08 +0900

    Modified readPasori using MaybeT

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,8 @@ import Text.JSON
 import Text.JSON.Generic 
 import Network.HTTP.Client
 import Control.Monad
+import Control.Monad.Trans.Maybe
+import Control.Monad.IO.Class
 
 -- 今はnameだけど将来的にcardIdにします
 data PostJSON = PostJSON {
@@ -39,38 +41,22 @@ main = do
   pasoriToThrow
   
 pasoriToThrow = do
-  cardId <- readPasori
-  print cardId
+  cardId <- runMaybeT readPasori
+  print $ fromJust cardId
   --throwRequest $ C.pack $ encode $ toJSON $ PostJSON $ C.unpack cardId
 
 fcfId = snd . splitAt 2 . fst . splitAt 9
 
 fcfRetake = snd . splitAt 14
 
-readPasori :: IO (ByteString, ByteString)
+
+readPasori :: MaybeT IO (ByteString)
 readPasori = do
-  maybePasori <- pasoriPrepare
-  case maybePasori of
-    Just pasori -> do
-      maybeFelicaPtr <- felicaPolling 0xfe00 0 0 pasori 
-      let felicaPtr = fromJust maybeFelicaPtr
-      felica <- withForeignPtr felicaPtr peek 
-      print "IDm is:"
-      print felica
-      print "PMm is:"
-      print $ pmm felica
-      maybeBlockWord <- withForeignPtr felicaPtr $ felicaReadSingle 0 0x1A8B 0
-      maybeBlockWord2 <- withForeignPtr felicaPtr $ felicaReadSingle 0 0x1A8B 1
-      maybeBlockWord3 <- withForeignPtr felicaPtr $ felicaReadSingle 0 0x1A8B 2
-      maybeBlockWord4 <- withForeignPtr felicaPtr $ felicaReadSingle 0 0x1A8B 3
-      let cardId = fcfRetake $ convert "SHIFT-JIS" "UTF-8" $ pack $ fromJust maybeBlockWord 
-      let cardName = convert "SHIFT-JIS" "UTF-8" $ pack $ fromJust maybeBlockWord2
-      let block3 = convert "SHIFT-JIS" "UTF-8" $ pack $ fromJust maybeBlockWord3
-      let block4 = convert "SHIFT-JIS" "UTF-8" $ pack $ fromJust maybeBlockWord
-      print block3
-      print block4
-      pasoriClose pasori
-      return (cardId, cardName)
-    Nothing -> error "Pasori is not connected"
-
-
+      pasori <- MaybeT pasoriPrepare
+      felicaPtr <- MaybeT $ felicaPolling 0xfe00 0 0 pasori 
+      felica <- liftIO $ withForeignPtr felicaPtr peek 
+      blockWord <- MaybeT $ withForeignPtr felicaPtr $ felicaReadSingle 0 0x1A8B 0
+      let cardId = fcfId $ convert "SHIFT-JIS" "UTF-8" $ pack blockWord
+      liftIO $ print cardId
+      liftIO $ pasoriClose pasori
+      return cardId
